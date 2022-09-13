349c7c914b7b0a39c05aff1f0b9dbaabe24f5e7c
Mgbos <o.chiemela@yahoo.com>
2022-09-12 23:19:54 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,13 @@
 
 int main(void)
 {
-	int i;
+	int i
 
-	for (i = 0 ; i < 10; i++)
-
-	{
-		printf("%i", i);
+	for (i = 48 ; i <= 57; i++)
+        {
+		putchar (i);
 	}
-	printf("\n");
+	putchar ('\n');
 
 	return (0);
 }
